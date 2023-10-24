2a61930c7e352ba715a70dde755f6b86113e950b
Miles602 <milesmd1426@gmail.com>
2023-10-23 23:39:38 -0400

    Login-Register
    Added working Login and Register page that brings to a fake Home page it is connected by MangoDB stores and retrieves credentials it also has a skeleton for Classes Collection and Posts Collection

--- 4155_Capstone_Project-main/client/src/components/Login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Login = () => {
-    return <div>Login Page Content</div>;
-};
-
-export default Login;
--- 4155_Capstone_Project-main/client/src/components/NewReview.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NewReview = () => {
-    return <div>New Review Page Content</div>;
-};
-
-export default NewReview;
